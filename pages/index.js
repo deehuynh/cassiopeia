27b279b78f1d05c7ad559a487d1381b76b83ca3d
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-09 23:28:08 +0700

    modified pages/index and add new meta file

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,8 @@
-import Head from 'next/head'
+
 
 export default function Layout() {
   return (
     <div className="layout">
-      <Head>
-        <title>Cassiopeia | Flower Store</title>
-        <meta 
-          name="description" 
-          content="The beautiful & fresh flower store" 
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       layout
     </div>
   )
