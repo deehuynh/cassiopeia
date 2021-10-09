7378e7fda037a4388459a07b43f124e48337647a
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-09 23:28:58 +0700

    modified cpn meta

---
+++ components/meta.js
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function Meta () {
+  return (
+    <Head>
+      <title>Cassiopeia | Flower Store</title>
+      <meta 
+        name="description" 
+        content="The beautiful & fresh flower store" 
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
