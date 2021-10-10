01a18e48fcf827fdf2396eadfb63a246e2fa2740
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-10 23:34:16 +0700

    modified pages/index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import Layout from "../components/layout"
 import Title from "../components/title"
 
 export default function Home () {
   return (
-    <Layout>
+    <>
       <Title>Cassiopeia | Flower Store</Title>
 
       Home
-    </Layout>
+    </>
   )
 }
