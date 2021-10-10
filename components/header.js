484f7a61f01a5e79edd5f215c8edf07a315c1bb4
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-10 23:55:11 +0700

    modified header cpn, add some block

--- components/header.js
+++ components/header.js
@@ -2,6 +2,13 @@
 
 export default function Header () {
   return (
-    <header className="header"></header>
+    <header className="header">
+      <div className="header__location"></div>
+      <div className="header__logo"></div>
+      <div className="header__group">
+        <div className="header__search"></div>
+        <div className="header__cart"></div>
+      </div>
+    </header>
   )
 }
