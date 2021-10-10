dba3e29ddbfeafe29ccf5bcac8cce6db01893eec
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-10 23:29:14 +0700

    import header to layout

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,12 @@
 import Meta from "./meta"
+import Header from "./header"
 
 export default function Layout ({ children }) {
   return (
     <div className="layout">
       <Meta />
+      <Header />
+      
       {children}
     </div>
   )
