ab1b1ca27012a7672ebceebf2e9d271144618c60
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-10 23:32:35 +0700

    import footer to layout

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,16 @@
 import Meta from "./meta"
 import Header from "./header"
+import Footer from "./footer"
 
 export default function Layout ({ children }) {
   return (
     <div className="layout">
       <Meta />
       <Header />
-      
+
       {children}
+
+      <Footer />
     </div>
   )
 }
