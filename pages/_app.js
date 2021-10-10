cc8473a521f119106a8075bc752ac82d6c14f398
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-10 23:35:49 +0700

    import layout to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
+// Global styles
 import '../styles/global/globals.css'
 import '../styles/scss/index.scss'
 
+// Components
+import Layout from '../components/layout'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
