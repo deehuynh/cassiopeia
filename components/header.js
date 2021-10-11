b8c4949ede0fa966d6231250d1a2c6f1d13f1eea
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-11 17:49:48 +0700

    modified header

--- components/header.js
+++ components/header.js
@@ -1,10 +1,12 @@
-
+import Image from "next/image"
 
 export default function Header () {
   return (
     <header className="header">
       <div className="header__location"></div>
-      <div className="header__logo"></div>
+      <div className="header__logo">
+        <img src="/logo.svg" alt="Logo" />
+      </div>
       <div className="header__group">
         <div className="header__search"></div>
         <div className="header__cart"></div>
