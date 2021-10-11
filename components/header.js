133577ae112abcde0084ea1ad9e7a3eff9fd1199
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-11 18:13:16 +0700

    added header contents

--- components/header.js
+++ components/header.js
@@ -3,13 +3,20 @@ import Image from "next/image"
 export default function Header () {
   return (
     <header className="header">
-      <div className="header__location"></div>
+      <div className="header__location">
+        <img src="/svgs/location.svg" alt="location icon" />
+        <span>Vietnam</span>
+      </div>
       <div className="header__logo">
         <img src="/svgs/logo.svg" alt="Logo" />
       </div>
       <div className="header__group">
-        <div className="header__search"></div>
-        <div className="header__cart"></div>
+        <div className="header__search">
+          <img src="/svgs/search.svg" alt="search icon" />
+        </div>
+        <div className="header__cart">
+          <img src="/svgs/cart.svg" alt="cart icon" />
+        </div>
       </div>
     </header>
   )
