1007dd92e976bddd73e57dff582d24181ef6af02
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-12 22:56:04 +0700

    create nav data

--- components/navigation.js
+++ components/navigation.js
@@ -1,5 +1,13 @@
 
 
+const elements = [
+  {name: 'Flowers', url: '/flowers'},
+  {name: 'Plants', url: '/plants'},
+  {name: 'Gifts', url: '/gifts'},
+  {name: 'Disconts', url: '/disconts'},
+  {name: 'About us', url: '/about-us'}
+];
+
 export default function Nav () {
   return (
     <nav className="nav"></nav>
