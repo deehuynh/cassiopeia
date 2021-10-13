ab144719d1a3e8b7c8dd2379fd204a683dfbe3d4
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-13 23:30:54 +0700

    footer ui

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,33 @@
-
+import Link from "next/link"
 
 export default function Footer () {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <div className="footer__row-1"></div>
+        <div className="footer__row-1">
+          <div className="footer__col-1"></div>
+
+          <div className="footer__col-2">
+            <div className="footer__link">
+              <Link href='/'>
+                <a></a>
+              </Link>
+            </div>
+
+            <div className="footer__link">
+              <Link href='/'>
+                <a></a>
+              </Link>
+            </div>
+
+            <div className="footer__link">
+              <Link href='/'>
+                <a></a>
+              </Link>
+            </div>
+          </div>
+        </div>
+        
         <div className="footer__row-2"></div>
       </div>
     </footer>
