04aba4f13440a1e356c4cff2f164348d3774316b
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-13 23:27:46 +0700

    footer ui

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,10 @@
 export default function Footer () {
   return (
     <footer className="footer">
-      <div className="footer__container"></div>
+      <div className="footer__container">
+        <div className="footer__row-1"></div>
+        <div className="footer__row-2"></div>
+      </div>
     </footer>
   )
 }
