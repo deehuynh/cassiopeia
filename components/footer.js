dc0d5b9dd1ea5c05e713bce8e9b2a50eb1506c44
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-13 23:40:06 +0700

    building footer ui

--- components/footer.js
+++ components/footer.js
@@ -15,28 +15,23 @@ export default function Footer () {
     <footer className="footer">
       <div className="footer__container">
         <div className="footer__row-1">
-          <div className="footer__col-1"></div>
+          <div className="footer__col-1">
+            <p>Follow us</p>
+
+            <span>+00 0000 0000</span>
+          </div>
 
           <div className="footer__col-2">
             <div className="footer__link">
               <p></p>
-
-              <Tab></Tab>
             </div>
 
             <div className="footer__link">
               <p></p>
-
             </div>
 
             <div className="footer__link">
               <p></p>
-
-              <span>
-                <Link href='/'>
-                  <a></a>
-                </Link>
-              </span>
             </div>
           </div>
         </div>
