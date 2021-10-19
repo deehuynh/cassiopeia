a247d042896de5c814e08cf7db3b9b963c6332ef
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-19 23:34:05 +0700

    update images

--- components/banner-ads.js
+++ components/banner-ads.js
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Img1 from "../public/images/cass15.jpg"
-import Img2 from "../public/images/cass21.jpg"
-import Img3 from "../public/images/cass3.jpg"
+import Img2 from "../public/images/cass22.jpg"
+import Img3 from "../public/images/cass23.png"
 
 const adsAPI = [
   {type: 'primary', background: Img1, title: '40% off', description: 'Best deals this week. Fresh flowers, plants and gifts', btnTitle: 'Shop now'},
