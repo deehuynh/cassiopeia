4096efec4c240dd3d316c3473718dfba7b6a0e0c
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-20 23:26:54 +0700

    setup slideshow

--- components/banner-ads.js
+++ components/banner-ads.js
@@ -1,4 +1,19 @@
 import Image from "next/image"
+import {Slide} from "react-slideshow-image"
+
+const slideImages = [
+  "/images/cass15.jpg",
+  "images/cass23.png",
+  "images/cass31.png"
+];
+
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  prevArrow: <div style={{width: "30px", marginRight: "-30px"}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff"><path d="M242 180.6v-138L0 256l242 213.4V331.2h270V180.6z"/></svg></div>,
+  nextArrow: <div style={{width: "30px", marginLeft: "-30px"}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff"><path d="M512 256L270 42.6v138.2H0v150.6h270v138z"/></svg></div>
+};
 
 const adsAPI = [
   {
