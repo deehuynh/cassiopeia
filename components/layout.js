f200369604cfb294cd3ea5adfb3f0e2e9ea2feae
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-24 23:35:17 +0700

    importing modal container

--- components/layout.js
+++ components/layout.js
@@ -6,6 +6,7 @@ import Meta from "./meta"
 import Header from "./header"
 import Nav from "./navigation"
 import MobileSearch from "./mobile-search"
+import ModalContainer from "./modal-container"
 import Footer from "./footer"
 
 export default function Layout ({ children }) {
