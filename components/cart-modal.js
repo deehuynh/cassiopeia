7617c619fc978056fef2478abd967cd9f520c433
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-27 06:06:04 +0700

    modified cart modal

--- components/cart-modal.js
+++ components/cart-modal.js
@@ -1,4 +1,4 @@
-
+import Link from "next/link"
 
 export default function Cart () {
   return (
@@ -7,11 +7,11 @@ export default function Cart () {
         Your cart
         <img src="/svgs/close-btn.svg" alt="close btn" />
       </h2>
-
       <Item />
       <Item />
-
       <Promocode />
+      <OrderTotal />
+      <CheckoutButton />
     </div>
   )
 }
@@ -42,5 +42,34 @@ function Promocode () {
       <input type="text" defaultValue='' placeholder='Add promocode' />
       <div className="cart-modal__promocode-btn">Apply</div>
     </div>
+  )
+}
+
+function OrderTotal () {
+  return (
+    <div className="cart-modal__order-total">
+      <div className="cart-modal__field">
+        <span>Shipping</span>
+        <span>FREE</span>
+      </div>
+
+      <div className="cart-modal__field">
+        <span>Order total</span>
+        <span>$00.00</span>
+      </div>
+    </div>
+  )
+}
+
+function CheckoutButton () {
+  return (
+    <Link href="/">
+      <a>
+        <div className="cart-modal__checkout-btn">
+          <span>Checkout</span>
+          <img src="/svgs/line-right-arrow.svg" alt="right arrow" />
+        </div>
+      </a>
+    </Link>
   )
 }
