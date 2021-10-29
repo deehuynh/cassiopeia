88e44799b6fa63cfadbfd4f2052487c82e32ad90
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-29 23:49:36 +0700

    modified pages disconts

---
+++ pages/disconts/index.js
@@ -0,0 +1,11 @@
+import Title from "../../components/title"
+
+export default function Disconts () {
+  return (
+    <div className="disconts">
+      <Title>Disconts | Cassiopeia | Flower store</Title>
+
+      Disconts
+    </div>
+  )
+}
