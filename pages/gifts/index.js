0e17fc63c52d925c981a0bd9651fac8f4c57dc4d
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-29 23:45:57 +0700

    modified pages gifts

---
+++ pages/gifts/index.js
@@ -0,0 +1,11 @@
+import Title from "../../components/title"
+
+export default function Gifts () {
+  return (
+    <div className="gifts">
+      <Title>Gifts | Cassiopeia | Flower store</Title>
+
+      Gifts
+    </div>
+  )
+}
