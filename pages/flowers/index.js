303f5d060e1eb665bf29e13a42ee215bc71ab844
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-29 23:40:50 +0700

    modified pages flowers

--- pages/flowers/index.js
+++ pages/flowers/index.js
@@ -1,7 +1,11 @@
-
+import Title from "../../components/title"
 
 export default function Flowers () {
   return (
-    <div className="flowers"></div>
+    <div className="flowers">
+      <Title>Flowers | Cassiopeia | Flower store</Title>
+
+      Flowers
+    </div>
   )
 }
