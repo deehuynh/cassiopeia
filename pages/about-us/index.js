b208474df59cd5d08a53a8bb37a95c1a48107eea
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-29 23:53:01 +0700

    modified pages about us

---
+++ pages/about-us/index.js
@@ -0,0 +1,11 @@
+import Title from "../../components/title"
+
+export default function AboutUs () {
+  return (
+    <div className="about-us">
+      <Title>About us | Cassiopeia | Flower store</Title>
+
+      About us
+    </div>
+  )
+}
