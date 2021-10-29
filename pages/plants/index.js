1c33665d8816de9a10d5e4c99d83eea9a073e05b
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-10-29 23:43:52 +0700

    modified pages: added plants page

---
+++ pages/plants/index.js
@@ -0,0 +1,11 @@
+import Title from "../../components/title"
+
+export default function Plants () {
+  return (
+    <div className="plants">
+      <Title>Plants | Cassiopeia | Flower store</Title>
+
+      Plants
+    </div>
+  )
+}
