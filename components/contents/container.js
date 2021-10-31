56339dcc000b494326e40ba1d314263ffb0251dc
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-01 05:38:14 +0700

    modified contents container: imported splidejs

--- components/contents/container.js
+++ components/contents/container.js
@@ -1,26 +1,41 @@
 import Item from "./item"
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 
 export default function Container (props) {
   const className = props.className && props.className !== '' ? ' ' + props.className : '';
   const listItem = [];
   props.prAPI && props.prAPI.forEach((item, index) => {
     listItem.push(
-      <Item 
-        name={item.name} thumbnail={item.thumbnail} price={item.price}
-        oldPrice={item.oldPrice} key={index} 
-      />
+      <SplideSlide key={index}>
+        <Item 
+          name={item.name} thumbnail={item.thumbnail} price={item.price}
+          oldPrice={item.oldPrice}
+        />
+      </SplideSlide>
     );
   });
 
   if (listItem.length > 3) {
     listItem.push(
-      <Item key={listItem.length} seeMore />
+      <SplideSlide key={listItem.length}>
+        <Item seeMore />
+      </SplideSlide>
     );
   }
 
   return (
     <div className={`content__container` + className}>
-      {listItem}
+      <Splide
+        options={{
+          type: 'slide',
+          width: '100%',
+          perPage: 4,
+          perMove: 1,
+          pagination: false
+        }}
+      >
+        {listItem}
+      </Splide>
     </div>
   )
 }
