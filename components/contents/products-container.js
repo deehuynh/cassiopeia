985505e612bec3f294f16820b697e94f878615d7
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-03 03:53:15 +0700

    created products container cpn

---
+++ components/contents/products-container.js
@@ -0,0 +1,39 @@
+// components
+import Item from "../contents/item"
+
+export default function ProductsContainer ({allProducts}) {
+  // data storaged variable
+  const products = [];
+  // data fetching
+  allProducts && allProducts.forEach((item, index) => {
+    products.push(
+      <Item 
+        key={index}
+        className='content__item--mgr-0'
+        thumbnail={item.thumbnail}
+        name={item.name}
+        price={item.price}
+        oldPrice={item.oldPrice}
+      />
+    );
+  });
+  return (
+    <div className="products-container">
+      {products}
+
+      {
+        allProducts && allProducts.length > 8 ? (
+          <SeeMore />
+        ) : ''
+      }
+    </div>
+  )
+}
+
+function SeeMore () {
+  return (
+    <div className="products-container__seemore">
+      <span>See more</span>
+    </div>
+  )
+}
