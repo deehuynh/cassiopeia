88caa2947e0824d9464701ba0e85520e13c95351
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-02 10:48:41 +0700

    modified contents breadcrumb

--- components/contents/breadcrumb.js
+++ components/contents/breadcrumb.js
@@ -1,7 +1,22 @@
-
+// next api
+import Link from "next/link"
 
 export default function BreadCrumb (props) {
+  const tabName = props.tabName;
   return (
-    <div className="breadcrumb"></div>
+    <div className="breadcrumb">
+      <Tab url='/' tabName='Home' />
+      <Tab url={tabName.url} tabName={tabName.name} />
+    </div>
+  )
+}
+
+function Tab (props) {
+  return (
+    <Link href={props.url}>
+      <a className="breadcrumb__tab">
+        {props.tabName}
+      </a>
+    </Link>
   )
 }
