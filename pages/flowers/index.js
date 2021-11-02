40661f1fc2f94382ae5ee24ff943169fcc72a957
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-02 10:47:58 +0700

    modified pages flowers: added breadcrumb

--- pages/flowers/index.js
+++ pages/flowers/index.js
@@ -1,11 +1,15 @@
+// components
 import Title from "../../components/title"
+import BreadCrumb from "../../components/contents/breadcrumb"
 
 export default function Flowers () {
   return (
     <div className="flowers">
       <Title>Flowers | Cassiopeia | Flower store</Title>
 
-      Flowers
+      <div className="flowers">
+        <BreadCrumb tabName={{url: '/flowers', name: 'Flowers'}} />
+      </div>
     </div>
   )
 }
