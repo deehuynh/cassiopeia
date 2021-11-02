5f7d914bc8ef63451b27fbbd9f4ffef11a13ab9c
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-03 03:54:56 +0700

    modified breadcrumb

--- components/contents/breadcrumb.js
+++ components/contents/breadcrumb.js
@@ -1,12 +1,44 @@
 // next api
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 export default function BreadCrumb (props) {
-  const tabName = props.tabName;
+  // tab name passed from detail page
+  const detailTab = props.detailTab ? props.detailTab : 'details';
+  // tabs
+  const tabs = [<Tab key={0} url='/' tabName='Home' />];
+  // access router object
+  const router = useRouter();
+  // path link
+  const pathlink = router.pathname;
+  // query param
+  const queryParam = router.query;
+  // name of path link
+  const names = [
+    {name: 'Flowers', path: 'flowers'},
+    {name: 'Plants', path: 'plants'},
+    {name: 'Gifts', path: 'gifts'},
+  ];
+  // comparing pathname
+  names.forEach((item)=>{
+    if (`/` + item.path === pathlink) {
+      tabs.push(
+        <Tab key={1} url={pathlink} tabName={item.name} />
+      );
+    }
+
+    // page detail's path name
+    if (queryParam.page && queryParam.page === item.path) {
+      tabs.push(
+        <Tab key={1} url={`/` + queryParam.page} tabName={item.name} />,
+        <Tab key={2} url={`/` + queryParam.page + '/' + queryParam.detail} tabName={detailTab} />
+      );
+    }
+  });
+
   return (
     <div className="breadcrumb">
-      <Tab url='/' tabName='Home' />
-      <Tab url={tabName.url} tabName={tabName.name} />
+      {tabs}
     </div>
   )
 }
