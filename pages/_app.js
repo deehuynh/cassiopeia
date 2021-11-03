033cdf22fb412d58a819c66cc7a045d175fb0801
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-04 05:01:16 +0700

    modified pages app: imported global style

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 // Global styles
 import '../styles/global/globals.css'
 import '../styles/scss/index.scss'
+// splidejs
+import '@splidejs/splide/dist/css/splide.min.css';
 
 // Using layout for entire pages
 import Layout from '../components/layout'
