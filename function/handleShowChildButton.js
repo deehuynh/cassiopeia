7187cfe8bb9c2b3d8ccf6b26e5b66e183d4db965
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-05 06:48:27 +0700

    modified function handleShowChildButton

--- function/handleShowChildButton.js
+++ function/handleShowChildButton.js
@@ -1,12 +1,15 @@
-export default function handleShowChildButton (firstRef, secondRef) {
+export default function handleShowChildButton (firstRef, secondRef, thirdRef) {
   const classFirstRef = firstRef.current;
   const classSecondRef = secondRef.current;
+  const classThirdRef = thirdRef.current;
 
   if (classFirstRef.className === 'show' && classSecondRef.className === 'hiden') {
     classFirstRef.className = 'hiden';
     classSecondRef.className = 'show';
+    classThirdRef.className = 'product-detail__list-children';
   } else {
     classFirstRef.className = 'show';
     classSecondRef.className = 'hiden';
+    classThirdRef.className = 'product-detail__list-children product-detail__list-children--hidden';
   }
 }
