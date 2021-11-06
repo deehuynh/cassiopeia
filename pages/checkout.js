01fce4679d70385b3fb9c62fe1fd73a6b4df5559
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-06 20:22:06 +0700

    modified pages checkout: added title and breadcrumb

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,5 +1,14 @@
+// head tags
+import Title from "../components/title"
+// components
+import BreadCrumb from "../components/contents/breadcrumb"
+
 export default function Checkout () {
   return (
-    <div className="checkout"></div>
+    <div className="checkout">
+      <Title>Checkout | Cassiopeia | Flower store</Title>
+
+      <BreadCrumb />
+    </div>
   )
 }
