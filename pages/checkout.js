236f9092282a55c98b50fa560396e733c52a001f
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-07 07:06:23 +0700

    modified pages checkout: added checkout content & order container cpn

--- pages/checkout.js
+++ pages/checkout.js
@@ -5,6 +5,19 @@ import BreadCrumb from "../components/contents/breadcrumb"
 import PageName from "../components/contents/page-name"
 
 export default function Checkout () {
+  // component partials
+  const OrderContainer = () => {
+    return (
+      <div className="checkout__order"></div>
+    )
+  }
+
+  const CheckoutContent = () => {
+    return (
+      <div className="checkout__content"></div>
+    )
+  }
+
   return (
     <div className="checkout">
       <Title>Checkout | Cassiopeia | Flower store</Title>
@@ -12,8 +25,8 @@ export default function Checkout () {
       <BreadCrumb />
       <PageName>Checkout</PageName>
       <div className="checkout__container">
-        <div className="checkout__col">1</div>
-        <div className="checkout__col">2</div>
+        <CheckoutContent>1</CheckoutContent>
+        <OrderContainer />
       </div>
     </div>
   )
