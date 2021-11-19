f966d6f8705167fdc5dec70e1e76aec087f2951d
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-19 21:15:14 +0700

    modified meta: added touch

--- components/meta.js
+++ components/meta.js
@@ -10,6 +10,7 @@ export default function Meta () {
       />
       <link rel="manifest" href="/manifest.json" />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
     </Head>
   )
