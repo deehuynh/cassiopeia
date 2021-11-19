e73501033c39255d5ae14bd10664ac2f0ac7911c
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-19 21:12:01 +0700

    modified meta: added icon 32x32 and 16x16

--- components/meta.js
+++ components/meta.js
@@ -9,6 +9,8 @@ export default function Meta () {
         content="The beautiful & fresh flower store"
       />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
     </Head>
   )
