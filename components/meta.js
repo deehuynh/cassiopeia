25b2579919688dfae9c089071d48b393e82f9271
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-19 21:10:20 +0700

    modified meta: added mask icon

--- components/meta.js
+++ components/meta.js
@@ -9,6 +9,7 @@ export default function Meta () {
         content="The beautiful & fresh flower store"
       />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
     </Head>
   )
 }
