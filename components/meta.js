5c35a9d4ef380233f013076cc027f6ac14f19604
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-20 18:56:02 +0700

    modified meta

--- components/meta.js
+++ components/meta.js
@@ -9,9 +9,11 @@ export default function Meta () {
         content="The beautiful & fresh flower store"
       />
       <link rel="manifest" href="/manifest.json" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   )
 }
