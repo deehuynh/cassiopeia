428dc6402db837e728cf3fb46f33bf06efdddced
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-21 20:27:43 +0700

    modified

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@
 import '../styles/global/globals.css'
 import '../styles/scss/index.scss'
 // splidejs
-import '@splidejs/splide/dist/css/splide.min.css';
+import '@splidejs/splide/dist/css/splide.min.css'
 
 // Using layout for entire pages
 import Layout from '../components/layout'
