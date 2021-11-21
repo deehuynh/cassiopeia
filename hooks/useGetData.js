c61c59c3cd3618893118183f249f22e4bd4deec6
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-22 00:18:22 +0700

    modified hooks useGetData

--- hooks/useGetData.js
+++ hooks/useGetData.js
@@ -2,23 +2,29 @@
 import { useEffect, useState } from "react"
 // firebase
 import { dbRef } from "../firebase"
-import { ref, get, child } from "firebase/database"
+import { get, child } from "firebase/database"
 
-function useGetData () {
+function useGetData (primaryKey) {
   // product list state
   const [productsArray, setProductsArray] = useState([]);
 
   useEffect(() => {
     get(child(dbRef, primaryKey)).then((snapshot) => {
       if (snapshot.exists()) {
-        console.log(snapshot.val());
+        const tmpStorage = [];
+        snapshot.forEach((item) => {
+          tmpStorage.push(item.val());
+        });
+        setProductsArray(tmpStorage);
       } else {
         console.log("No data available");
       }
     }).catch((error) => {
         console.error(error);
     });
-  })
+  }, [primaryKey])
 
   return productsArray;
 }
+
+export default useGetData;
