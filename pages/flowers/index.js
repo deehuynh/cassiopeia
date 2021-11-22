d7d8a2da6bd7500eb8f57bc968e553abf4f59a6c
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-22 21:35:16 +0700

    modified pages flowers

--- pages/flowers/index.js
+++ pages/flowers/index.js
@@ -60,23 +60,6 @@ export async function getStaticProps() {
     },
   ];
 
-  const allProductsT = [
-    {
-      name: 'White Lilies and Gerberas', price: '54', oldPrice: '',
-      thumbnail: '/5cdd463408a93217111334_xbpxkx.webp'
-    },
-    {
-      name: 'Red Roses and White Lilies', price: '99', oldPrice: '',
-      thumbnail: '/5d84dc1a631b2292689077_nihv8m.webp'
-    },
-    {
-      name: 'Chrysanthemums and Roses', price: '44', oldPrice: '',
-      thumbnail: '/5d19dc8cc0983744838000_xc256j.webp'
-    },
-    {name: 'Roses and Lilies', price: '69', oldPrice: '82', thumbnail: '/5d84d46e1fe63737087781_eztymk.webp'},
-    {name: 'Lilies And Roses', price: '39', oldPrice: '50', thumbnail: '/5d84d53800517236157520_awcivk.webp'}
-  ];
-
   const res = await fetch(`https://dh-cassiopeia-default-rtdb.asia-southeast1.firebasedatabase.app/flowers.json`);
   const allProducts = await res.json();
 
