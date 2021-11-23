29bea4aa9e5e45430bef239910dbcc5579156312
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-23 21:03:07 +0700

    modified pages aboutUS

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -1,4 +1,9 @@
+// the head tag's title
 import Title from "../../components/title"
+// components
+import BreadCrumb from "../../components/contents/breadcrumb"
+import PageName from "../../components/contents/page-name"
+import FilterBar from "../../components/contents/filter-bar"
 
 export default function AboutUs () {
   return (
