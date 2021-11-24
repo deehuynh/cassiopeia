dfb6c468db0345fb6d292a9f400527502e5bbcc7
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-24 20:24:02 +0700

    modified pages about-us

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -26,7 +26,27 @@ export default function AboutUs () {
 function AboutUsContent () {
   return (
     <div className="about-us__content">
+      <p>
+        The best way to brighten the special day of a friend or loved
+        one is with flowers. But what happens when that special someone
+        is far away? With Cassiopeia it is possible to surprise someone
+        with a lovely bouquet no matter where he or she is in the world.
+      </p>
 
+      <p>
+        With over 15 years of experience, we have covered many
+        requests and are always receptive to all of your ideas,
+        greeted with enthusiasm and our can-do attitude - if
+        we aim to achieve something then we will!
+      </p>
+
+      <p>
+        Over the course of the last 15 years we have grown steadily
+        thanks to the trust placed in us by our customers.
+        Today, We are a leading company in the world for sending flowers.
+        We are also able to speedily ship fresh flowers to more than 20
+        countries around the world.
+      </p>
     </div>
   )
 }
