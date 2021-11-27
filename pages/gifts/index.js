e92571f1439f2069f94ac8e342347a98998eef78
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-28 00:56:54 +0700

    modified pages gifts: imported components

--- pages/gifts/index.js
+++ pages/gifts/index.js
@@ -1,11 +1,18 @@
+// head tag's title
 import Title from "../../components/title"
+// components
+import BreadCrumb from "../../components/contents/breadcrumb"
+import ProductsContainer from "../../components/contents/products-container"
+import FilterBar from "../../components/contents/filter-bar"
 
 export default function Gifts () {
   return (
     <div className="gifts">
       <Title>Gifts | Cassiopeia | Flower store</Title>
 
-      Gifts
+      <BreadCrumb />
+      <FilterBar />
+      <ProductsContainer />
     </div>
   )
 }
