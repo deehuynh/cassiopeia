99a5d55e1815e70706fdb2dd56eb32de66e96fb0
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-28 15:53:39 +0700

    modified api relevantPrApi: fixed error

--- api/relevantPrApi.js
+++ api/relevantPrApi.js
@@ -1,12 +1,15 @@
 export default function relevantPrApi (flowersData) {
+  const countFlowersData = flowersData.length
   // storaged end data
   const relevantFlowers = [];
   // array of random number
   const randomNumber = [];
+  // fixed error: total product is less than 5, while loop will be infinity
+  const totalProduct = countFlowersData < 5 ? countFlowersData : 5
 
-  while (randomNumber.length < 5) {
+  while (randomNumber.length < totalProduct) {
     // get a random number
-    const randomNumberTmp = Math.floor((Math.random() * flowersData.length) + 0)
+    const randomNumberTmp = Math.floor((Math.random() * countFlowersData) + 0)
     
     if (randomNumber.length === 0) {
       // push the first random number
