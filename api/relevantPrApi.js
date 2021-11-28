d1b3b516a47e73448b4edcc451b5f0687d78545a
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-28 19:47:56 +0700

    modified api relevantPrApi

--- api/relevantPrApi.js
+++ api/relevantPrApi.js
@@ -1,45 +1,59 @@
-export default function relevantPrApi (flowersData) {
+export default function relevantPrApi (flowersData, existedId) {
   const countFlowersData = flowersData.length
   // storaged end data
-  const relevantFlowers = [];
+  const relevantFlowers = []
   // array of random number
-  const randomNumber = [];
+  const randomNumber = []
   // fixed error: total product is less than 5, while loop will be infinity
-  const totalProduct = countFlowersData < 5 ? countFlowersData : 5
+  // countFlowersData - 1: without existedId
+  const totalProduct = countFlowersData <= 5 ? countFlowersData - 1 : 5
 
   while (randomNumber.length < totalProduct) {
     // get a random number
     const randomNumberTmp = Math.floor((Math.random() * countFlowersData) + 0)
-    
+
     if (randomNumber.length === 0) {
       // push the first random number
-      randomNumber.push(randomNumberTmp)
+      if (randomNumberTmp != existedId) {
+        randomNumber.push(randomNumberTmp)
+      }
     } else {
       switch (randomNumber.length) {
         // random number must be different from existed number in array
         case 1 :
           if (randomNumberTmp !== randomNumber[0]) {
-            randomNumber.push(randomNumberTmp)
+            // don't get the currently viewing product
+            if (randomNumberTmp != existedId) {
+              randomNumber.push(randomNumberTmp)
+            }
           }
           break
         case 2:
           if ((randomNumberTmp !== randomNumber[0]) && (randomNumberTmp !== randomNumber[1])) {
-            randomNumber.push(randomNumberTmp)
+            if (randomNumberTmp != existedId) {
+              randomNumber.push(randomNumberTmp)
+            }
           }
           break
         case 3:
           if ((randomNumberTmp !== randomNumber[0]) && (randomNumberTmp !== randomNumber[1]) && (randomNumberTmp !== randomNumber[2])) {
-            randomNumber.push(randomNumberTmp)
+            if (randomNumberTmp != existedId) {
+              randomNumber.push(randomNumberTmp)
+            }
           }
           break
         case 4:
           if ((randomNumberTmp !== randomNumber[0]) && (randomNumberTmp !== randomNumber[1]) && (randomNumberTmp !== randomNumber[2]) && (randomNumberTmp !== randomNumber[3])) {
-            randomNumber.push(randomNumberTmp)
+            if (randomNumberTmp != existedId) {
+              randomNumber.push(randomNumberTmp)
+            }
           }
           break
         case 5:
           if ((randomNumberTmp !== randomNumber[0]) && (randomNumberTmp !== randomNumber[1]) && (randomNumberTmp !== randomNumber[2]) && (randomNumberTmp !== randomNumber[3]) && (randomNumberTmp !== randomNumber[4])) {
-            randomNumber.push(randomNumberTmp)
+            if (randomNumberTmp != existedId) {
+              randomNumber.push(randomNumberTmp)
+            }
           }
           break
       }
