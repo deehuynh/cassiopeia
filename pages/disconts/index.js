80cdd971794cfc6886c9813ee0a15a9174e55eb2
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-11-28 21:11:22 +0700

    modified pages disconts: imported breadcrumb

--- pages/disconts/index.js
+++ pages/disconts/index.js
@@ -1,11 +1,14 @@
+// head tag's title
 import Title from "../../components/title"
+// component
+import Breadcrumb from "../../components/contents/breadcrumb"
 
 export default function Disconts () {
   return (
     <div className="disconts">
       <Title>Disconts | Cassiopeia | Flower store</Title>
 
-      Disconts
+      <Breadcrumb />
     </div>
   )
 }
