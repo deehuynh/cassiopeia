465ea1950fe80bf2fbad1b30b831f5ab736d5044
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-02 00:07:15 +0700

    modified item: added effect

--- components/contents/item.js
+++ components/contents/item.js
@@ -78,6 +78,7 @@ export default function Item (props) {
 function OfferStock ({children}) {
   return (
     <div className="content__item-offer-stock">
+      <span></span>
       {children}
     </div>
   )
