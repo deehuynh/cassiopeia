1bd13560fc3a8d6e0ea65e700a74e9d6cc5d40d4
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-02 21:39:12 +0700

    modified reducers detail-pr-reducer: handled count amount product

--- reducers/detail-pr-reducer.js
+++ reducers/detail-pr-reducer.js
@@ -1,9 +1,21 @@
 export default function detailProductReducer (state, action) {
   switch (action.type) {
     case 'increase_amount_pr':
-      return state
-    case 'descrease_amount_pr':
-      return state
+      if (state.amount < 100) {
+        return {
+          amount: state.amount + 1
+        }
+      } else {
+        return {amount: state.amount}
+      }
+    case 'decrease_amount_pr':
+      if (state.amount > 1) {
+        return {
+          amount: state.amount - 1
+        }
+      } else {
+        return {amount: state.amount}
+      }
     default:
       throw new Error()
   }
