4ca00211b1980c0d71c6592ae456008640df3edd
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-03 22:35:44 +0700

    modified localStorage

--- localStorage/handleAddToCart.js
+++ localStorage/handleAddToCart.js
@@ -1,3 +1,12 @@
-export default function handleAddToCart (itemData) {
-  localStorage.setItem('cart', itemData)
+export default function handleAddToCart (itemData = {}) {
+  let itemStorage = []
+
+  if (!localStorage.getItem('cart')) {
+    itemStorage.push(itemData)
+    localStorage.setItem('cart', JSON.stringify(itemStorage))
+  } else {
+    itemStorage = JSON.parse(localStorage.getItem('cart'))
+    itemStorage.push(itemData)
+    localStorage.setItem('cart', JSON.stringify(itemStorage))
+  }
 }
