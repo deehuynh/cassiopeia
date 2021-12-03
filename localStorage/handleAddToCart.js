1dbe4a17aa79cdd1f0d4579b0cc38d3f923581ed
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-03 23:19:26 +0700

    modified localStorage handleAddToCart

--- localStorage/handleAddToCart.js
+++ localStorage/handleAddToCart.js
@@ -1,18 +1,20 @@
 export default function handleAddToCart (itemData = {}) {
-  // storaged items as an array
-  let itemStorage = []
+  if (typeof window !== 'undefined') {
+    // storaged items as an array
+    let itemStorage = []
 
-  // check the first added product
-  if (!localStorage.getItem('cart')) {
-    itemStorage.push(itemData)
-    localStorage.setItem('cart', JSON.stringify(itemStorage))
-  } else {
-    // not first product
-    // get an currently items array
-    itemStorage = JSON.parse(localStorage.getItem('cart'))
-    // push new item to items array
-    itemStorage.push(itemData)
-    // set localStorage
-    localStorage.setItem('cart', JSON.stringify(itemStorage))
+    // check the first added product
+    if (!localStorage.getItem('cart')) {
+      itemStorage.push(itemData)
+      localStorage.setItem('cart', JSON.stringify(itemStorage))
+    } else {
+      // not first product
+      // get an currently items array
+      itemStorage = JSON.parse(localStorage.getItem('cart'))
+      // push new item to items array
+      itemStorage.push(itemData)
+      // set localStorage
+      localStorage.setItem('cart', JSON.stringify(itemStorage))
+    }
   }
 }
