1e0e772575121251f7c7f3707caec69b201145dc
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-06 01:44:30 +0700

    modified pages app: provide store to app

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,19 @@ import '../styles/global/globals.css'
 import '../styles/scss/index.scss'
 // splidejs
 import '@splidejs/splide/dist/css/splide.min.css'
-
 // Using layout for entire pages
 import Layout from '../components/layout'
+// redux
+import store from '../store'
+import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   )
 }
 
