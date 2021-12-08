2093964700165d1a953568196ce50b1c0da0727c
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-08 21:09:14 +0700

    modified modal-container: passed order cart ref prop

--- components/modal-container.js
+++ components/modal-container.js
@@ -4,7 +4,7 @@ export default function ModalContainer (props) {
   return (
     <div className="modal-container">
       <Cart 
-        cartRef={props.cartRef}
+        cartRef={props.cartRef} orderCartRef={props.orderCartRef}
         openCartRef={props.openCartRef} closeCartRef={props.closeCartRef}
       />
     </div>
