5f7abff3902d7194708ae3a07028b6d2c6e6d935
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-10 20:07:00 +0700

    modified cart-modal: added direct link to item thumbnail overlay

--- components/cart-modal.js
+++ components/cart-modal.js
@@ -64,9 +64,11 @@ function Item ({storagedItems}) {
     listItem.push(
       <div key={index} className="cart-modal__item">
         <div className="cart-modal__avatar">
-          <a className="cart-modal__avatar-overlay">
-            <img src="/svgs/view-btn.svg" alt="view detail" />
-          </a>
+          <Link href={`/${item.page}/${item.id}`}>
+            <a className="cart-modal__avatar-overlay">
+              <img src="/svgs/view-btn.svg" alt="view detail" />
+            </a>
+          </Link>
           <Image src={item.thumbnail} width={100} height={100} alt="thumbnail" />
         </div>
         <div className="cart-modal__infor">
