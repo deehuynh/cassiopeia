bb72a58afed4092c1c98dcfebb88eb317c6e70c8
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-16 17:49:27 +0700

    modified modal-container

--- components/modal-container.js
+++ components/modal-container.js
@@ -1,11 +1,20 @@
 import Cart from "./cart-modal"
 
 export default function ModalContainer (props) {
+  const overlayRef = props.overlayModalRef
+
   return (
     <div className="modal-container">
+      <div 
+        ref={overlayRef} 
+        className="modal-container__overlay modal-container__overlay--hidden"
+        id="modal-container__overlay"
+      ></div>
+
       <Cart 
         cartRef={props.cartRef} orderCartRef={props.orderCartRef}
         openCartRef={props.openCartRef} closeCartRef={props.closeCartRef}
+        overlayModalRef={overlayRef}
       />
     </div>
   )
