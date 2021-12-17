f81202d4e69351898598d28d978d291972f4c94e
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-18 00:05:21 +0700

    modified mobile-search: handled input value state

--- components/mobile-search.js
+++ components/mobile-search.js
@@ -1,7 +1,15 @@
+import useSearch from "../hooks/useSearch"
+
 export default function MobileSearch (props) {
+  const [searchValue, setSearchValue] = useSearch()
+
   return (
     <div ref={props.searchRef} className="m-search__hiden">
-      <input type="text" defaultValue='' placeholder='Search' />
+      <input
+        type="text" placeholder='Search' 
+        value={searchValue}
+        onChange={setSearchValue}
+      />
     </div>
   )
 }
