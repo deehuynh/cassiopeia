15272918c97ead42121dc0d85d75a3bbe34ef8ac
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-18 20:47:14 +0700

    modified mobile-search

--- components/mobile-search.js
+++ components/mobile-search.js
@@ -1,14 +1,14 @@
 import useSearch from "../hooks/useSearch"
 
 export default function MobileSearch (props) {
-  const [searchValue, setSearchValue] = useSearch()
+  const {searchValue, handleOnChangeValue} = useSearch()
 
   return (
     <div ref={props.searchRef} className="m-search__hiden">
       <input
         type="text" placeholder='Search' 
         value={searchValue}
-        onChange={setSearchValue}
+        onChange={handleOnChangeValue}
       />
     </div>
   )
