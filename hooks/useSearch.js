1305943cd15adc729b0b61453a941d84b77ab708
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-18 16:00:18 +0700

    modified hooks useSearch: handled data fetching

--- hooks/useSearch.js
+++ hooks/useSearch.js
@@ -1,12 +1,26 @@
 import { useState, useEffect } from "react"
 
 function useSearch () {
-  // search state
+  // search value state
   const [searchState, setSearchState] = useState('')
+  // all data state
+  const [data, setData] = useState(null)
   // handle onChange value
   const handleOnChangeValue = (e) => {
     setSearchState(e.target.value)
   }
+
+  useEffect(() => {
+    async function fetchAPI () {
+      const url = 'https://dh-cassiopeia-default-rtdb.asia-southeast1.firebasedatabase.app/.json'
+      const res = await fetch(url)
+      const data = await res.json()
+      
+      setData(data)
+    }
+
+    fetchAPI()
+  }, [])
 
   return [searchState, handleOnChangeValue]
 }
