b56dbe639c690c2dbef188a11f96c0167bccd93d
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-20 23:19:29 +0700

    modified search-input: trying search function use redux

--- components/search-input.js
+++ components/search-input.js
@@ -1,7 +1,19 @@
+// hooks
 import useSearch from "../hooks/useSearch"
+// redux toolkit
+import { useDispatch, useSelector } from "react-redux"
+import { setValue } from "../redux/searchSlice"
 
 export default function SearchInput ({searchRef}) {
-  const {searchValue, handleOnChangeValue} = useSearch()
+  // const {searchValue, handleOnChangeValue} = useSearch()
+  // redux dispatch
+  const dispatch = useDispatch()
+  // get search value
+  const searchValue = useSelector(state => state.search.value)
+  // handle on change value
+  const handleOnChangeValue = ({target: {value}}) => dispatch(
+    setValue(value)
+  )
 
   return (
     <input
