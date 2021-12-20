8d6091908b6b2d9823480fca6bc8f88be94a7b18
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-20 23:38:17 +0700

    modified redux searchSlice: handled setSearchKey

--- components/search-input.js
+++ components/search-input.js
@@ -1,19 +1,32 @@
+// react api
+import { useRef } from "react"
 // hooks
 import useSearch from "../hooks/useSearch"
 // redux toolkit
 import { useDispatch, useSelector } from "react-redux"
-import { setValue } from "../redux/searchSlice"
+import { setValue, setSearchKey } from "../redux/searchSlice"
 
 export default function SearchInput ({searchRef}) {
   // const {searchValue, handleOnChangeValue} = useSearch()
+  // the value will be persisted on every render by useRef
+  const typingTimeout = useRef(null)
   // redux dispatch
   const dispatch = useDispatch()
   // get search value
   const searchValue = useSelector(state => state.search.value)
   // handle on change value
-  const handleOnChangeValue = ({target: {value}}) => dispatch(
-    setValue(value)
-  )
+  const handleOnChangeValue = ({target: {value}}) => {
+    // set value
+    dispatch(setValue(value));
+    // is typing, reset time to 0
+    if (typingTimeout.current) {
+      clearTimeout(typingTimeout.current)
+    }
+    // submit after already typing
+    typingTimeout.current = setTimeout(() => {
+      dispatch(setSearchKey(value))
+    }, 500)
+  }
 
   return (
     <input
