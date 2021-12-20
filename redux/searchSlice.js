d630a33623604b9c90fa6dd1592a0844da846409
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-20 23:15:24 +0700

    modified redux searchSlice: trying search function use redux

--- redux/searchSlice.js
+++ redux/searchSlice.js
@@ -2,14 +2,22 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const searchSlice = createSlice({
   name: 'search',
-  initialState: [],
+  initialState: {
+    value: '',
+    data: []
+  },
   reducers: {
     getItems: (state, action) => {
-      return action.payload
+      state.data = action.payload
+    },
+
+    setValue: (state, action) => {
+      // get input value
+      state.value = action.payload
     }
   }
 })
 
-export const { getItems } = searchSlice.actions
+export const { getItems, setValue } = searchSlice.actions
 
 export default searchSlice.reducer
