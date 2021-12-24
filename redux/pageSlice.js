b7815f161d6d93a1ce17502ad59db67bcbcf9f28
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-24 23:28:15 +0700

    modified redux pageSlice: handled sortBy price low to high reducer

--- redux/pageSlice.js
+++ redux/pageSlice.js
@@ -8,15 +8,31 @@ const pageSlice = createSlice({
     gifts: []
   },
   reducers: {
-    orderBy: (state, action) => {
-      switch (action.payload.orderBy) {
+    sortBy: (state, action) => {
+      const pageName = action.payload.pageName
+      const pageData = action.payload.pageData
+      const sortBy = action.payload.sortBy
+      switch (sortBy) {
         case 'priceLowToHigh':
-          return state
+          let swap = false
+          let tmp
+          for (let i = 0; i < pageData.length - 1; i++) {
+            swap = false
+            for (let j = i + 1; j < pageData.length; j++) {
+              if (Number(pageData[i].price) > Number(pageData[j].price)) {
+                tmp = pageData[i]
+                pageData[i] = pageData[j]
+                pageData[j] = tmp
+                swap = true
+              }
+            }
+          }
+        return {...state, [pageName]: pageData}
       }
     }
   }
 })
 
-export const { orderBy } = pageSlice.actions
+export const { sortBy } = pageSlice.actions
 
 export default pageSlice.reducer
