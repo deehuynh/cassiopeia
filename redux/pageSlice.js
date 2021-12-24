cb3079f10bd82171d61028f4f1cf33c1ee295a0d
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-25 01:58:44 +0700

    modified redux pageSlice: handled sort by price high to low

--- redux/pageSlice.js
+++ redux/pageSlice.js
@@ -3,16 +3,27 @@ import { createSlice } from "@reduxjs/toolkit"
 // pure functions
 const sortByPrice = (pageData, type) => {
   if (type === 'priceLowToHigh') {
-    let swap = false
     let tmp
     for (let i = 0; i < pageData.length - 1; i++) {
-      swap = false
       for (let j = i + 1; j < pageData.length; j++) {
         if (Number(pageData[i].price) > Number(pageData[j].price)) {
           tmp = pageData[i]
           pageData[i] = pageData[j]
           pageData[j] = tmp
-          swap = true
+        }
+      }
+    }
+    return pageData
+  }
+
+  if (type === 'priceHighToLow') {
+    let tmp
+    for (let i = 0; i < pageData.length - 1; i++) {
+      for (let j = i + 1; j < pageData.length; j++) {
+        if (Number(pageData[i].price) < Number(pageData[j].price)) {
+          tmp = pageData[i]
+          pageData[i] = pageData[j]
+          pageData[j] = tmp
         }
       }
     }
@@ -32,11 +43,9 @@ const pageSlice = createSlice({
       const pageName = action.payload.pageName
       const pageData = action.payload.pageData
       const sortBy = action.payload.sortBy
-      switch (sortBy) {
-        case 'priceLowToHigh':
-          const handledData = sortByPrice(pageData, sortBy)
-        return {...state, [pageName]: handledData}
-      }
+      const handledData = sortByPrice(pageData, sortBy)
+
+      return {...state, [pageName]: handledData}
     }
   }
 })
