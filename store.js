80bf7d78a5806480429edd91a103648e7729795a
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-24 22:27:08 +0700

    added pageSlice

--- store.js
+++ store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./redux/cartSlice"
 import searchSlice from "./redux/searchSlice";
+import pageSlice from "./redux/pageSlice";
 
 export default configureStore({
   reducer: {
     cart: cartReducer,
-    search: searchSlice
+    search: searchSlice,
+    pages: pageSlice
   }
 })
