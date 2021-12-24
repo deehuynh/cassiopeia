349890bf67c8f87f7cbe34117e50edd381a4c592
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-24 22:34:27 +0700

    modified redux pageSlice

--- redux/pageSlice.js
+++ redux/pageSlice.js
@@ -2,7 +2,11 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const pageSlice = createSlice({
   name: 'pages',
-  initialState: []
+  initialState: {
+    flowers: [],
+    plants: [],
+    gifts: []
+  },
 })
 
 export default pageSlice.reducer
