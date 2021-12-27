8f43b90a7e5a0c89132c87a03389978b13f56efa
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-27 19:37:35 +0700

    modified redux handledSelectByPrice: handled for page plants

--- redux/pure_functions/handleSelectByPrice.js
+++ redux/pure_functions/handleSelectByPrice.js
@@ -1,41 +1,76 @@
 import handleSortBy from "./handleSortBy"
 
 export default function handleSelectByPrice(page, data, option) {
-  const flowersPricing = ["Under $10", "$10 - $50", "$50 - $100", "Over $100"]
-  const plantsPricing = ["Under $20", "$20 - $70", "$70 - $100", "Over $100"]
-  const giftsPricing = ["Under $50", "$50 - $100", "Over $100"]
   const endData = []
-  if (option === "Under $10") {
-    data.forEach((item) => {
-      if (Number(item.price) < 10) {
-        endData.push(item)
-      }
-    })
 
-    return handleSortBy(endData, 'Low to high')
-  } else if (option === "$10 - $50") {
-    data.forEach((item) => {
-      if ((Number(item.price) >= 10) && (Number(item.price) < 50)) {
-        endData.push(item)
-      }
-    })
-
-    return handleSortBy(endData, 'Low to high')
-  } else if (option === "$50 - $100") {
-    data.forEach((item) => {
-      if ((Number(item.price) >= 50) && (Number(item.price) < 100)) {
-        endData.push(item)
-      }
-    })
-
-    return handleSortBy(endData, 'Low to high')
-  } else if (option === "Over $100") {
-    data.forEach((item) => {
-      if (Number(item.price) > 50) {
-        endData.push(item)
-      }
-    })
-
-    return handleSortBy(endData, 'Low to high')
+  if (page === 'flowers') {
+    if (option === "Under $10") {
+      data.forEach((item) => {
+        if (Number(item.price) < 10) {
+          endData.push(item)
+        }
+      })
+  
+      return handleSortBy(endData, 'Low to high')
+    } else if (option === "$10 - $50") {
+      data.forEach((item) => {
+        if ((Number(item.price) >= 10) && (Number(item.price) < 50)) {
+          endData.push(item)
+        }
+      })
+  
+      return handleSortBy(endData, 'Low to high')
+    } else if (option === "$50 - $100") {
+      data.forEach((item) => {
+        if ((Number(item.price) >= 50) && (Number(item.price) < 100)) {
+          endData.push(item)
+        }
+      })
+  
+      return handleSortBy(endData, 'Low to high')
+    } else if (option === "Over $100") {
+      data.forEach((item) => {
+        if (Number(item.price) > 100) {
+          endData.push(item)
+        }
+      })
+  
+      return handleSortBy(endData, 'Low to high')
+    }
+  } else if (page === 'plants') {
+    if (option === "Under $20") {
+      data.forEach((item) => {
+        if (Number(item.price) < 20) {
+          endData.push(item)
+        }
+      })
+  
+      return handleSortBy(endData, 'Low to high')
+    } else if (option === "$20 - $70") {
+      data.forEach((item) => {
+        if ((Number(item.price) >= 20) && (Number(item.price) < 70)) {
+          endData.push(item)
+        }
+      })
+  
+      return handleSortBy(endData, 'Low to high')
+    } else if (option === "$70 - $100") {
+      data.forEach((item) => {
+        if ((Number(item.price) >= 70) && (Number(item.price) < 100)) {
+          endData.push(item)
+        }
+      })
+  
+      return handleSortBy(endData, 'Low to high')
+    } else if (option === "Over $100") {
+      data.forEach((item) => {
+        if (Number(item.price) > 100) {
+          endData.push(item)
+        }
+      })
+  
+      return handleSortBy(endData, 'Low to high')
+    }
   }
+
 }
