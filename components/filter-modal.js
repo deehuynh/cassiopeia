72f6f7011e8ea3e2a84a2ee004b9f56a87fa5eb8
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-27 22:41:42 +0700

    modified filter-modal: style close button

--- components/filter-modal.js
+++ components/filter-modal.js
@@ -5,6 +5,12 @@ import Link from "next/link"
 export default function FilterModal () {
   return (
     <div className="filter-modal">
+      <header className="filter-modal__header">
+        <div className="filter-modal__close-btn">
+          <img src="/svgs/close-btn.svg" alt="close" />
+        </div>
+      </header>
+
       <Link href="/">
         <a className="filter-modal__tab">
           <span>Sort by</span>
