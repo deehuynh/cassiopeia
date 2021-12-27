55b21aa28a8b4341ae77950cf2622dfe56f503d2
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-27 22:22:16 +0700

    modified filter-modal

--- components/filter-modal.js
+++ components/filter-modal.js
@@ -1,3 +1,37 @@
+/* eslint-disable @next/next/no-img-element */
+// next api
+import Link from "next/link"
+
 export default function FilterModal () {
-  return ()
+  return (
+    <div className="filter-modal">
+      <Link href="/">
+        <a className="filter-modal__tab">
+          <span>Sort by</span>
+          <img src="/svgs/dropdown-i.svg" alt="dropdown arrow" />
+        </a>
+      </Link>
+
+      <Link href="/">
+        <a className="filter-modal__tab">
+          <span>Sort by</span>
+          <img src="/svgs/dropdown-i.svg" alt="dropdown arrow" />
+        </a>
+      </Link>
+
+      <Link href="/">
+        <a className="filter-modal__tab">
+          <span>Sort by</span>
+          <img src="/svgs/dropdown-i.svg" alt="dropdown arrow" />
+        </a>
+      </Link>
+
+      <Link href="/">
+        <a className="filter-modal__tab">
+          <span>Sort by</span>
+          <img src="/svgs/dropdown-i.svg" alt="dropdown arrow" />
+        </a>
+      </Link>
+    </div>
+  )
 }
