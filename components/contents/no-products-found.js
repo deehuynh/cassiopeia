41b1ae9f5577c7104629390868840c85172f122b
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-29 23:02:45 +0700

    modified contents no-products-found

--- components/contents/no-products-found.js
+++ components/contents/no-products-found.js
@@ -1,7 +1,7 @@
-export default function NoProductsFound () {
+export default function NoProductsFound ({text}) {
   return (
     <div className="no-prs-found">
-      <span>No Products Found</span>
+      <span>No Products Found {text}</span>
     </div>
   )
 }
