360bb92a177cf59f5532be4667d7ad10451a874a
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-12-31 08:52:04 +0700

    imported checkoutSlice

--- store.js
+++ store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./redux/cartSlice"
 import searchSlice from "./redux/searchSlice";
 import pageSlice from "./redux/pageSlice";
+import checkoutSlice from "./redux/checkoutSlice";
 
 export default configureStore({
   reducer: {
     cart: cartReducer,
     search: searchSlice,
-    pages: pageSlice
+    pages: pageSlice,
+    checkout: checkoutSlice
   }
 })
