71bd8beb81dbd2f181f85a96ffa2dc8a4bf18a87
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2022-01-18 20:06:53 +0700

    modified pages about-us

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -1,6 +1,7 @@
 // the head tag's title
 import Title from "../../components/title"
 import Description from "../../components/description"
+import OpenGraph from "../../components/open-graph"
 // components
 import BreadCrumb from "../../components/contents/breadcrumb"
 import PageName from "../../components/contents/page-name"
@@ -18,6 +19,15 @@ export default function AboutUs () {
         We are also able to speedily ship fresh flowers to more than 20
         countries around the world.
       </Description>
+      <OpenGraph 
+        name="About us | Cassiopeia"
+        desc="Over the course of the last 15 years we have grown steadily
+          thanks to the trust placed in us by our customers.
+          Today, We are a leading company in the world for sending flowers.
+          We are also able to speedily ship fresh flowers to more than 20
+          countries around the world."
+        image="https://res.cloudinary.com/didlxgowc/image/upload/f_auto,c_limit,w_1080,q_auto/252351058_10158633590988277_7917329218506464257_n.jpg_scglja.jpg"
+      />
 
       <BreadCrumb tabName={{url: '/about-us', name: 'About us'}} />
       <PageName>About us</PageName>
