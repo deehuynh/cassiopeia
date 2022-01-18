5b6021417d6f3761a4afa6c257b4069d1a017a9b
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2022-01-18 20:06:21 +0700

    modified open-graph

--- components/open-graph.js
+++ components/open-graph.js
@@ -1,11 +1,10 @@
 import Head from "next/head"
 
-export default function OpenGraph ({name, type, url, image}) {
+export default function OpenGraph ({name, desc, image}) {
   return (
     <Head>
       <meta property="og:title" content={name} />
-      <meta property="og:type" content={type} />
-      <meta property="og:url" content={url} />
+      <meta property="og:description" content={desc} />
       <meta property="og:image" content={image} />
     </Head>
   )
